47b1ca594a60ad9c16a703c79d0d8c7e14458fea
harshh3104 <harshbhrucha05@gmail.com>
2025-11-08 11:47:50 +0530

    All scripts file

---
+++ scripts.php
@@ -0,0 +1,31 @@
+<!-- Vendor js -->
+<script src="assets/js/vendor.min.js"></script>
+
+<!-- App js -->
+<script src="assets/js/app.js"></script>
+
+<!-- Apex Chart js -->
+<script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+
+<!-- Projects Analytics Dashboard App js -->
+<script src="assets/js/pages/dashboard.js"></script>
+
+<!-- Datatables js -->
+<script src="assets/vendor/datatables/dataTables.min.js"></script>
+<script src="assets/vendor/datatables/dataTables.bootstrap5.min.js"></script>
+<script src="assets/vendor/datatables/dataTables.responsive.min.js"></script>
+<script src="assets/vendor/datatables/responsive.bootstrap5.min.js"></script>
+<script src="assets/vendor/datatables/fixedColumns.bootstrap5.min.js"></script>
+<script src="assets/vendor/datatables/dataTables.fixedHeader.min.js"></script>
+<script src="assets/vendor/datatables/dataTables.buttons.min.js"></script>
+<script src="assets/vendor/datatables/buttons.bootstrap5.min.js"></script>
+<script src="assets/vendor/datatables/buttons.html5.min.js"></script>
+<script src="assets/vendor/datatables/buttons.print.min.js"></script>
+<script src="assets/vendor/datatables/jszip.min.js"></script>
+<script src="assets/vendor/datatables/pdfmake.min.js"></script>
+<script src="assets/vendor/datatables/vfs_fonts.js"></script>
+<script src="assets/vendor/datatables/dataTables.keyTable.min.js"></script>
+<script src="assets/vendor/datatables/dataTables.select.min.js"></script>
+
+<!-- Datatable Demo js -->
+<script src="assets/js/components/table-datatable.js"></script>
